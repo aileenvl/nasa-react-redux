b18e854fd182a75b9bcc65fa238c53a34d4b2a18
aileenvl <aileenvl@gmail.com>
2017-10-15 17:31:37 -0500

    excercise to try react-redux and nasa api

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './assets/stylesheets/styles.scss';
+
+import React from 'react'; // import the main react dependency
+import ReactDOM from 'react-dom'; // import reactDOM
+import { Provider } from 'react-redux' // Glue for react and redux
+import Store from './Store'; // redux state store
+import App from './app/App'; // import the main app component
+
+ReactDOM.render(
+  <Provider store={Store}>{/*Glue for react and redux*/}
+    <App />{/* Render our App Component */}
+  </Provider>, 
+  document.getElementById('root')
+); // render our App component and mount it to our #root element
